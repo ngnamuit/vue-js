b26442615ef8bd9311ef06bff23efbe071a5b0e0
nam.nguyen <nam.nguyen@smartnetvn.com.vn>
2018-12-07 16:25:04 +0700

    routing demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Foo from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: __dirname,
+  forceRefresh: true,
+  routes: [
+    { path: '/foo', component: Foo, name: 'Foo'
+    }
+  ]
+})
+
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
-}).$mount('#app')
+
+})
